3988ec317831bf1a09147640e75c9fc0ead5be80
Alexandre <AlexandreOSouza>
2022-09-26 15:00:25 -0300

    add theme

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { Text } from "@chakra-ui/react";
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+      </Head>
+    </div>
+  );
+};
+
+export default Home;
