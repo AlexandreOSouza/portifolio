5e405952a37435e8ef9e989ade0fb280116b61d1
Alexandre <AlexandreOSouza>
2022-09-27 20:20:18 -0300

    initial commit

--- src/components/pages/content/about/index.tsx
+++ src/components/pages/content/about/index.tsx
@@ -1,7 +1,7 @@
 import { Text } from "@chakra-ui/react";
 
 const AboutMeContent = () => {
-  return <Text textStyle={"headline"}>About me</Text>;
+  return <Text textStyle={"headline"}></Text>;
 };
 
 export default AboutMeContent;
