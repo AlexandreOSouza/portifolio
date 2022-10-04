71daedc83d3458deeeab6e4537effe47a45f0288
Alexandre <AlexandreOSouza>
2022-10-04 13:21:52 -0300

    add cards container

---
+++ src/components/pages/content/projects/project-card/index.tsx
@@ -0,0 +1,16 @@
+import { Flex } from "@chakra-ui/react";
+
+const ProjectCard = () => {
+  return (
+    <Flex
+      width={"100%"}
+      maxWidth={"300px"}
+      height={"200px"}
+      borderRadius={"15px"}
+      border={"1px solid"}
+      borderColor={"lines.main"}
+    ></Flex>
+  );
+};
+
+export default ProjectCard;
--- src/components/pages/content/projects/projects-list/index.tsx
+++ src/components/pages/content/projects/projects-list/index.tsx
@@ -1,7 +1,18 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Grid, GridItem, Text } from "@chakra-ui/react";
 import { RiCloseFill } from "react-icons/ri";
+import ProjectCard from "../project-card";
 
 const ProjectsList = () => {
+  const ProjectRow = () => {
+    return (
+      <Flex width={"100%"} justifyContent={"center"} columnGap={10}>
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+      </Flex>
+    );
+  };
+
   return (
     <Flex flex={1} flexDirection={"column"}>
       <Flex
@@ -18,7 +29,33 @@ const ProjectsList = () => {
         <Text textStyle={"label"}>React; CSS</Text>
         <RiCloseFill />
       </Flex>
-      <Flex flex={1} borderTop={"1px solid"} borderColor={"lines.main"}></Flex>
+      <Box
+        display={"grid"}
+        gridTemplateColumns={{
+          base: "repeat(auto-fill, minmax(165px, 1fr))",
+          md: "repeat(auto-fill, minmax(230px, 1fr))",
+          xl: "repeat(auto-fill, minmax(260px, 1fr))",
+        }}
+        borderTop={"1px solid"}
+        borderColor={"lines.main"}
+        alignItems={"center"}
+        overflowY={"auto"}
+        rowGap={10}
+        columnGap={10}
+        height={"100%"}
+        py={"50px"}
+        px={"100px"}
+      >
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+      </Box>
     </Flex>
   );
 };
