d282ac750ef49e3db576c227dab68233011346b1
Alexandre <AlexandreOSouza>
2022-10-07 12:06:22 -0300

    fix drawer redirect

--- src/components/drawer/index.tsx
+++ src/components/drawer/index.tsx
@@ -4,7 +4,8 @@ import {
   DrawerContent,
   DrawerBody,
 } from "@chakra-ui/react";
-import { PAGE_INDEX } from "../../helper/constants";
+import { useRouter } from "next/router";
+import { PAGE_INDEX, PAGE_URL } from "../../helper/constants";
 import { usePage } from "../../hooks/usePages";
 import DrawerButton from "../CTA/drawer-button";
 type Props = {
@@ -13,10 +14,11 @@ type Props = {
 };
 
 const MenuDrawer = ({ isOpen, onClose }: Props) => {
-  const { currentPage, setCurrentPage } = usePage();
+  const router = useRouter();
+  const currentPage = router.asPath.replace("/", "");
 
   const handleClick = (pageIndex: number) => {
-    setCurrentPage(pageIndex);
+    router.push(`/${PAGE_URL[pageIndex as 1 | 2 | 3 | 4]}`);
     onClose();
   };
 
@@ -31,25 +33,25 @@ const MenuDrawer = ({ isOpen, onClose }: Props) => {
         >
           <DrawerBody p={0}>
             <DrawerButton
-              isActive={currentPage === PAGE_INDEX.HOME}
+              isActive={currentPage === PAGE_URL[1]}
               onClick={() => handleClick(PAGE_INDEX.HOME)}
             >
               _hello
             </DrawerButton>
             <DrawerButton
-              isActive={currentPage === PAGE_INDEX.ABOUT}
+              isActive={currentPage === PAGE_URL[2]}
               onClick={() => handleClick(PAGE_INDEX.ABOUT)}
             >
               _about-me
             </DrawerButton>
             <DrawerButton
-              isActive={currentPage === PAGE_INDEX.PROJECTS}
+              isActive={currentPage === PAGE_URL[3]}
               onClick={() => handleClick(PAGE_INDEX.PROJECTS)}
             >
               _projects
             </DrawerButton>
             <DrawerButton
-              isActive={currentPage === PAGE_INDEX.CONTACT}
+              isActive={currentPage === PAGE_URL[4]}
               onClick={() => handleClick(PAGE_INDEX.CONTACT)}
             >
               _contact-me
