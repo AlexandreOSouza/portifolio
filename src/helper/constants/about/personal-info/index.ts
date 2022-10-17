177343a63c1d84c93919103b0734fdee78615967
Alexandre <AlexandreOSouza>
2022-10-17 16:12:06 -0300

    add personal info content

--- src/helper/constants/about/personal-info/index.ts
+++ src/helper/constants/about/personal-info/index.ts
@@ -1,6 +1,10 @@
 const PersonalInfo = `
 1&nbsp;&nbsp;/**</br>
-2&nbsp;&nbsp;&nbsp;* Personal Info</br>
-16&nbsp;&nbsp;*/`;
+2&nbsp;&nbsp;&nbsp;* 👋 Hi, I am Alexandre Souza (The Dev LOL) 🧑‍💻 </br>
+3&nbsp;&nbsp;&nbsp;* I am a fullstack Developer current work as </br>
+4&nbsp;&nbsp;&nbsp;* Software Developer at @Popstand. </br>
+5&nbsp;&nbsp;&nbsp;* I'm from Brazil(🇧🇷) and passionate about blockchain and  </br>
+6&nbsp;&nbsp;&nbsp;* software development. </br>
+7&nbsp;&nbsp; */`;
 
 export default PersonalInfo;
