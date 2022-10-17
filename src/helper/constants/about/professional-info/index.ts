dfc16812cb342af159b21422199555c65c1e8435
Alexandre <AlexandreOSouza>
2022-10-17 18:37:41 -0300

    add professional info content

--- src/helper/constants/about/professional-info/index.ts
+++ src/helper/constants/about/professional-info/index.ts
@@ -1,19 +1,19 @@
 const ProfessionalInfo = `
-1&nbsp;&nbsp;/**</br>
-2&nbsp;&nbsp;&nbsp;* I'm a Fullstack Developer, and I already </br>
-2&nbsp;&nbsp;&nbsp;* work with some cool programming languages as  </br>
-2&nbsp;&nbsp;&nbsp;* - [x] JavaScript (React JS & React Native)  </br>
-2&nbsp;&nbsp;&nbsp;* - [x] Solidity </br>
-2&nbsp;&nbsp;&nbsp;* - [x] Java </br>
-2&nbsp;&nbsp;&nbsp;* - [x] Python </br>
-2&nbsp;&nbsp;&nbsp;* Right now my main priority is on Web Development </br>
-2&nbsp;&nbsp;&nbsp;* focus in React JS and the Blockchain Development with  </br>
-2&nbsp;&nbsp;&nbsp;* Solidity</br>
-2&nbsp;&nbsp;&nbsp;*</br>
-2&nbsp;&nbsp;&nbsp;* In my dev journey (5 yr) I work as Fullstack,</br>
-2&nbsp;&nbsp;&nbsp;* Backend and Frontend Dev, and also I spend some  </br>
-2&nbsp;&nbsp;&nbsp;* good months just working with Database</br>
-2&nbsp;&nbsp;&nbsp;* (MongoDB & SQLServer)</br>
-16&nbsp;&nbsp;*/`;
+&nbsp;1&nbsp;&nbsp;/**</br>
+&nbsp;2&nbsp;&nbsp;&nbsp;* 👋 Hi, I am Alexandre Souza 🧑‍💻</br>
+&nbsp;3&nbsp;&nbsp;&nbsp;* </br>
+&nbsp;4&nbsp;&nbsp;&nbsp;* Frontend Dev based in Brazil(🇧🇷), with </br>
+&nbsp;5&nbsp;&nbsp;&nbsp;* experience in web and mobile app development. </br>
+&nbsp;6&nbsp;&nbsp;&nbsp;* Blockchain is my new passion, I have been working    </br>
+&nbsp;7&nbsp;&nbsp;&nbsp;* with some decentralized projects like NFT marketplaces  </br>
+&nbsp;8&nbsp;&nbsp;&nbsp;* and DAPP projects in general. </br>
+&nbsp;9&nbsp;&nbsp;&nbsp;* </br>
+&nbsp;9&nbsp;&nbsp;&nbsp;* For your projects I bring more than 5 years of experience</br>
+10&nbsp;&nbsp;&nbsp;* with multiple technologies such as:</br>
+11&nbsp;&nbsp;&nbsp;* ReactJS, React Native, Python, Java </br>
+12&nbsp;&nbsp;&nbsp;* and database in general.</br>
+13&nbsp;&nbsp;&nbsp;* I also bring great communication, teamwork and speed </br>
+14&nbsp;&nbsp;&nbsp;* without wasting time and money</br>
+15&nbsp;&nbsp;&nbsp;*/`;
 
 export default ProfessionalInfo;
